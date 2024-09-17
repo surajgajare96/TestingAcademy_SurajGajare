a4cb7d182eac302eb818fd47b5386b31e619a753
Suraj Gajare <surajgajare96@gmail.com>
2024-09-17 12:22:56 +0530

    Latest

--- src/Sep_17/dataTypes.java
+++ src/Sep_17/dataTypes.java
@@ -1,4 +1,17 @@
 package Sep_17;
 
 public class dataTypes {
+
+    public static void main(String[] args) {
+
+
+
+    String welcome = "Welcome to my repository";
+
+        System.out.println(welcome);
+
+
+
+    }
+
 }
